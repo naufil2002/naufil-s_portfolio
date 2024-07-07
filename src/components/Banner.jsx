04cda9cbae0076e7bf6877b74a5cdedfcdcb9778
Naufil Shaikh <141832951+naufil2002@users.noreply.github.com>
2024-07-07 19:25:08 +0530

    updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-// import HeaderImg from "../assets/img/header-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 import Typewriter from "typewriter-effect";
 import NavBar from "./NavBar";
-// import backgroundVideo from "../assets/bg-video.mp4"
-// import Introduction from "./Introduction";
 import HeaderImg from "../assets/img/header-img.svg";
 import IntroImg from "../assets/img/intro-img.svg";
 import { NavLink } from "react-router-dom";
+
 
 const Banner = () => {
   return (
     <>
       <NavBar />
-      {/* <video autoPlay muted loop id="background-video">
-      <source src={backgroundVideo} type="video/mp4" />
-      </video> */}
       <section className="banner" id="home">
         <Container>
           <Row className="align-items-center">
@@ -28,15 +22,14 @@ const Banner = () => {
                 {({ isVisible }) => (
                   <div
                     className={
-                      isVisible ? "animate__animated animate__fadeIn" : ""
+                      isVisible ? "animate__animated animate__fadeInLeft" : ""
                     }
                   >
                     <span className="tagline">Welcome to My Portfolio</span>
                     <h1>
-                      {" "}
                       Hi! I'm{" "}
                       <span className="purple">
-                        {` Naufil Shaikh.`}
+                        {`Naufil Shaikh.`}
                         <span className="wrap">
                           <Typewriter
                             options={{
@@ -51,13 +44,13 @@ const Banner = () => {
                             }}
                           />
                         </span>
-                      </span>{" "}
+                      </span>
                     </h1>
                     <p>
-                      I'm passionate about creating
-                      engaging web applications.  Seeking opportunity to apply my
-                      skills and knowledge to contribute to innovative projects
-                      and further develop my expertise
+                      I'm passionate about creating engaging web applications.
+                      Seeking opportunity to apply my skills and knowledge to
+                      contribute to innovative projects and further develop my
+                      expertise.
                     </p>
                     <NavLink
                       className="p-4 text-white text-decoration-none"
@@ -80,17 +73,17 @@ const Banner = () => {
           </Row>
         </Container>
         <Container className="containerr">
-          <Row className="align-items-center ">
+          <Row className="align-items-center">
             <Col xs={12} md={6} xl={7}>
               <TrackVisibility>
                 {({ isVisible }) => (
                   <div
                     className={
-                      isVisible ? "animate__animated animate__fadeIn" : ""
+                      isVisible ? "animate__animated animate__fadeInRight" : ""
                     }
                   >
                     <div>
-                      <h1 className=" mb-5 Intro1">
+                      <h1 className="mb-5 Intro1">
                         LET ME <span className="purple">INTRODUCE</span> MYSELF!
                       </h1>
                       <h5 className="Intro2">
@@ -102,13 +95,13 @@ const Banner = () => {
                       <h5 className="mt-5 Intro2">
                         I am fluent in classics like{" "}
                         <span className="purple fst-italic">
-                          JavaScript and it's library ReactJS
+                          JavaScript and its library ReactJS
                         </span>
                       </h5>
                       <h5 className="mt-5 Intro2">
-                        My field of Interest's are building new{" "}
+                        My field of interest is building new{" "}
                         <span className="purple fst-italic">
-                         Frontend Cross Platform Web Apps
+                          Frontend Cross Platform Web Apps
                         </span>{" "}
                         and also in areas related to{" "}
                         <span className="purple fst-italic">
@@ -126,7 +119,6 @@ const Banner = () => {
           </Row>
         </Container>
       </section>
-      {/* <Introduction /> */}
     </>
   );
 };
